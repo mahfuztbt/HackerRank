a8c7d53dafe45c4a8318074b402ef7c8aff98ece
MahfuzRonnie <rony@mail.ynu.edu.cn>
2020-08-03 21:43:50 +0600

    BST Level-Order Traversal

---
+++ 30 Days of Code/Day 23.py
@@ -0,0 +1,40 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        nodes_search = list()
+        nodes_traversed = ""
+        nodes_search.append(root)
+        while len(nodes_search) > 0:
+            node = nodes_search.pop(0)
+            if node.left:
+                nodes_search.append(node.left)
+            if node.right:
+                nodes_search.append(node.right)
+            nodes_traversed += str(node.data) + " "
+        print(nodes_traversed)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
