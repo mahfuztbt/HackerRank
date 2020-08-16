2a906c878fd8ae236d8d02638e4da4d71c68c3ea
MahfuzRonnie <rony@mail.ynu.edu.cn>
2020-08-16 11:58:26 +0600

    Nested Lists

---
+++ Python/Basic Data Types/010.py
@@ -0,0 +1,16 @@
+marksheet = []
+scoresheet = []
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        marksheet += [[name, score]]
+        scoresheet += [score]
+    x = sorted(set(scoresheet))[1]
+
+    for n, s in sorted(marksheet):
+        if s == x:
+            print(n)
+
+
