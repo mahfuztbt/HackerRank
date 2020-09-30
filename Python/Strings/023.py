e29ad3b2aaca4cf94aea57e2856aecb620f8aa1e
MahfuzRonnie <rony@mail.ynu.edu.cn>
2020-09-30 16:28:05 +0600

    String Formatting

---
+++ Python/Strings/023.py
@@ -0,0 +1,9 @@
+def print_formatted(number):
+    # your code goes here
+    w=len(str(bin(number))[2:])
+    for i in range(1, number+1):
+        print(str(i).rjust(w," "),oct(i)[2:].rjust(w," "),hex(i)[2:].upper().rjust(w," "),bin(i)[2:].rjust(w," "))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
