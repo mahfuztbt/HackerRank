e90c8b0ecd0f026c51934fd3db1e3e479c13ffd6
MahfuzRonnie <rony@mail.ynu.edu.cn>
2020-10-06 17:12:19 +0600

    Merge the Tools!

---
+++ Python/Strings/027.py
@@ -0,0 +1,14 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    c=0
+    s=""
+    for i in string:
+        if i not in s:
+            s=s+i
+        c+=1
+        if(c==k):
+            print(s)
+            c=0
+            s=""
+
+if __name__ == '__main__':
