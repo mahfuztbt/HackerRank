ad6805aadcc6b74b8de46526076a19d0df23669e
MahfuzRonnie <rony@mail.ynu.edu.cn>
2021-03-12 16:40:04 +0800

    Set .symmetric_difference() Operation

---
+++ Python/Sets/039.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == "__main__":
+    input()
+    A = set(map(int, input().split()))
+    
+    input()
+    B = set(map(int, input().split()))
+    
+    print(len(A.symmetric_difference(B)))
+
