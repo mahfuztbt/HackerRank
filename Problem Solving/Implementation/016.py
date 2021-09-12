d557766183dc875269e82ad443697d081acaab7c
mahfuztbt <rony@mail.ynu.edu.cn>
2021-09-12 18:35:53 +0800

    Subarray Division

---
+++ Problem Solving/Implementation/016.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    count = 0
+    total = sum(s[:m])
+    
+    if total == d:
+        count+=1
+        
+    for i in range(m, len(s)):
+        total += s[i]
+        total -= s[i-m]
+        if total == d:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
