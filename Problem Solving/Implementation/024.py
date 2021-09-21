6b1644e50fc8c19b15c293ab201cd3b2e2450dd7
mahfuztbt <rony@mail.ynu.edu.cn>
2021-09-21 18:22:46 +0800

    Electronics Shop

---
+++ Problem Solving/Implementation/024.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    totalPrice = 0
+    comboList = []
+    
+    for k in keyboards:
+        for d in drives:
+            totalPrice = k+d
+            if totalPrice <= b:
+                comboList.append(totalPrice)
+            else:
+                comboList.append(-1)
+    return max(comboList)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
