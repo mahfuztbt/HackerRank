203027589832773cf8cf3b5e0faf4fd43d55a24b
mahfuztbt <rony@mail.ynu.edu.cn>
2021-10-10 22:55:55 +0800

    Beautiful Days at the Movies

---
+++ Problem Solving/Implementation/033.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    count = 0
+    
+    for x in range(i, j+1):
+        rx = int(str(x)[::-1])
+        if abs(x-rx)%k==0:
+            count+=1
+    return count 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
