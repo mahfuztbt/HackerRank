89263877677de89988a707fa2fb0f0ba11cc5eb2
mahfuztbt <rony@mail.ynu.edu.cn>
2021-10-11 21:31:13 +0800

    Viral Advertising

---
+++ Problem Solving/Implementation/034.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    total_likes = 0
+    shared = 5
+    
+    for i in range(n):
+        like = shared//2
+        total_likes += like
+        shared = like * 3
+    
+    return total_likes
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
