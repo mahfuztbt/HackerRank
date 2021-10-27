dd629dad7bf0df089f3d69240b92ca194b7712cb
mahfuztbt <rony@mail.ynu.edu.cn>
2021-10-27 10:06:00 +0800

    Sherlock and Squares

---
+++ Problem Solving/Implementation/042.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from math import *
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    # Write your code here
+    return floor(sqrt(b)) - ceil(sqrt(a)) + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
