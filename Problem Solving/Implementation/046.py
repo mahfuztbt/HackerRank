52dbf75e4addb369cf4fb84319b78232a7162922
mahfuztbt <rony@mail.ynu.edu.cn>
2021-11-01 21:52:04 +0800

    Repeated String

---
+++ Problem Solving/Implementation/046.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    n1=(n//len(s))
+    x=s.count("a")
+    x1=n1*x
+    x2=s[:n%(len(s))].count('a')
+    return x1+x2
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
