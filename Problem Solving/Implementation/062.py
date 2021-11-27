e156e67dbb35f283fd4045cb92d365b4e219e4e0
mahfuztbt <rony@mail.ynu.edu.cn>
2021-11-27 20:59:43 +0800

    Lisa's Workbook

---
+++ Problem Solving/Implementation/062.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    # Write your code here
+    answer = 0
+    page = 1
+    
+    for problems in arr:
+        for index in range(1, problems+1):
+            if index == page:
+                answer += 1
+                
+            if index == problems or index % k == 0:
+                page += 1
+                
+    return answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
