3e55ca78884fcdfb5a3724c351fcac9c9420d3be
mahfuztbt <rony@mail.ynu.edu.cn>
2022-01-31 09:54:56 +0800

    Mars Exploration

---
+++ Problem Solving/Strings/004.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    count = 0
+    r = len(s)//3
+    target = "SOS"*r
+    
+    for i, j in zip(s, target):
+        if i != j:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
