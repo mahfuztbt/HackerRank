abc93ab0358b02ed2ee9f5a149ce8e844ef007e8
mahfuztbt <rony@mail.ynu.edu.cn>
2022-02-03 13:21:20 +0800

    HackerRank in a String!

---
+++ Problem Solving/Strings/005.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerrankInString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def hackerrankInString(s):
+    # Write your code here
+    target = "hackerrank"
+    n = len(target)
+    
+    i = 0
+    for char in s:
+        if char == target[i]:
+            i += 1
+            if i == n:
+                return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
