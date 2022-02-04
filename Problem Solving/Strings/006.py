75cfa33ea93c25e6df84c356c2c5cb0ab6f02fc9
mahfuztbt <rony@mail.ynu.edu.cn>
2022-02-04 21:32:18 +0800

    Pangrams

---
+++ Problem Solving/Strings/006.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    
+    temp = set(s.lower())-set(" ")
+    if len(temp) == 26:
+        return "pangram"
+    else:
+        return "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
