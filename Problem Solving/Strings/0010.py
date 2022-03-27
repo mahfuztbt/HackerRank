7d6520602b939c2b9e718f7f98188b5822ef155c
mahfuztbt <rony@mail.ynu.edu.cn>
2022-03-27 09:49:50 +0800

    Gemstones

---
+++ Problem Solving/Strings/0010.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+def gemstones(arr):
+    # Write your code here
+    result = set(arr[0])
+    
+    for i in range(1, n):
+        temp = set(arr[i])
+        
+        result = result.intersection(temp)
+    
+    return len(result)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
